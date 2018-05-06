1f98ff5659c79ac1b3215ba2ed5474b39b71a756
YoungDydlak <dawiddydlinski@gmail.com>
2018-05-07 00:20:32 +0200

    Github Pages faliet, remove docs directory

--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# PHP-HotelCenter
-My php school project for good grades cuz im retarded and school is hard;
-Bartekkur1 is helping me. Without him It would be 2 hard :)
--- docs/api/apis.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "api/DbController.php";
-include "api/HotelApi.php";
-include "api/ApartamentApi.php";
-include "api/AccountApi.php";
-include "api/FileApi.php";
-?>
